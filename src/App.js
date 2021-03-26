d176b779af29119d2c80d9ce8ae4db006874676e
Gyan <gyanadesh1998@gmail.com>
2021-03-26 13:21:06 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import {handlers} from "./mocks/handlers";
 export default function App() {
 
+console.log(handlers[0].info);
   return (
     <div className="App">
-	  <h1>hello world</h1>
+		    <select style={{backgroundColor:"lightGray",width:"500px",height:"40px"}}>
+		    	<option>Affenpinscher</option>
+		    	<option>African</option>
+				<option>Airedale</option>	
+				<option>Australian Shepherd</option> 
+			</select>
+			<br/>
+			<img src="https://images.dog.ceo/breeds/affenpinscher/n02110627_10437.jpg" alt="Dog image"/>
     </div>
   );
 }
